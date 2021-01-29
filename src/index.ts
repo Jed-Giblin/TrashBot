7893bf89d4e9dac7576ad07ce68796da14839d70
jed giblin <jed.giblin@odincc.net>
2021-01-29 11:11:49 -0500

    added memes

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,28 @@
 import {Telegraf, Context} from 'telegraf'
 import {ParseMode} from "telegraf/typings/telegram-types";
+import * as fs from "fs";
+import {openSync} from "fs";
 
 const token = process.env.TRASH_BOT_TOKEN || '';
 const bot = new Telegraf(token);
+interface ClientOpts {
+    words: string[],
+    opts: {
+        memes: boolean
+    }
+}
 interface Db {
-    [key: number]: RegExp[]
+    [key: number]: ClientOpts
 }
 
-const myDb: Db = {};
+var myDb: Db;
 
-// bot.start( (ctx: Context) => {
-//     ctx.reply('Im online');
-// });
-//
-// bot.hears("new game", (ctx: Context) => {
-//    ctx.reply("Starting a new game");
-// });
-//
-// bot.command('newgame', (ctx: Context) => {
-//    ctx.reply("Started a new game");
-// });
-//
-// bot.command('enter', (ctx: Context) => {
-//     if ( ctx.message !== undefined ) {
-//         if ( ctx.message.from !== undefined ) {
-//             ctx.reply(`Got it [${ctx.message.from.first_name}](tg://user?id=${ctx.message.from.id})`, {  parse_mode: "Markdown"})
-//         }
-//
-//     } else {
-//         ctx.reply("Message was undefined")
-//     }
-// });
-// let regexp: RegExp = /.*gulshan.*/;
-// bot.hears(regexp, (ctx: Context) => {
-//    let msg = ctx.message;
-//    if ( msg !== undefined ) {
-//
-//
-//    }
-// });
+if ( fs.existsSync('db/db.json') ) {
+    myDb = JSON.parse(fs.readFileSync('db/db.json').toString());
+} else {
+    myDb = {};
+}
+
 
 bot.command('newword', (ctx: Context) => {
    let message = ctx.message;
@@ -50,13 +34,11 @@ bot.command('newword', (ctx: Context) => {
            let realWord = realWords.join(" ");
            let chatid = message.chat.id;
            if ( myDb[chatid] === undefined ) {
-               myDb[chatid] = [];
-               let regex = new RegExp(`.*${realWord.toLowerCase()}.*`);
-               myDb[chatid].push(regex);
+               myDb[chatid] = { words: [], opts: { memes: false }};
+               myDb[chatid].words.push(`.*${realWord.toLowerCase()}.*`);
                ctx.reply(`Added a new trigger word: ${realWord}`);
            } else {
-               let regex = new RegExp(`.*${realWord.toLowerCase()}.*`);
-               myDb[chatid].push(regex);
+               myDb[chatid].words.push(`.*${realWord.toLowerCase()}.*`);
                ctx.reply(`Added a new trigger word: ${realWord}`);
            }
        } else {
@@ -67,26 +49,62 @@ bot.command('newword', (ctx: Context) => {
    }
 });
 
+bot.command('/meme', (ctx: Context) => {
+    let message = ctx.message;
+    if ( message !== undefined ) {
+        let chatId = message.chat.id;
+        if ( myDb[chatId] === undefined ) {
+            myDb[chatId] = { words: [], opts: { memes: true }};
+        } else {
+            myDb[chatId].opts.memes = true;
+        }
+
+        ctx.reply("Memes have been enabled for your chat room");
+    }
+});
+
 bot.on('message', (ctx: Context) => {
     let message = ctx.message;
     if ( message !== undefined ) {
         let chatid = message.chat.id;
-        if ( chatid !== undefined && myDb[chatid] !== undefined && myDb[chatid].length > 0 ) {
+        if ( chatid !== undefined && myDb[chatid] !== undefined && myDb[chatid].words.length > 0 ) {
             let msgText = message.text;
             if ( msgText !== undefined ) {
                 if ( msgText.startsWith("/") ) {
                     return;
                 }
-                for ( let i = 0; i < myDb[chatid].length; i++ ) {
-                    let r: RegExp = myDb[chatid][i];
+                for ( let i = 0; i < myDb[chatid].words.length; i++ ) {
+                    let r = new RegExp(myDb[chatid].words[i]);
                     if ( r.test(msgText.toLowerCase()) ) {
-                        ctx.reply("Trash!", {reply_to_message_id: message.message_id});
+                        if ( myDb[chatid].opts.memes ) {
+                            sendMeme(ctx, chatid, message.message_id);
+                        } else {
+                            ctx.reply("Trash!", {reply_to_message_id: message.message_id});
+                        }
                         return;
                     }
                 }
             }
         }
     }
+});
+
+function sendMeme(ctx: Context, chatId: Number, replyId: number) {
+    let memes = fs.readdirSync('media');
+    let meme_fn = memes[Math.floor(Math.random() * memes.length)];
+    ctx.replyWithPhoto({source: `media/${meme_fn}`}, {reply_to_message_id: replyId});
+}
+
+process.on('SIGTERM', () => {
+   console.log("Storing output");
+   fs.writeFileSync('db/db.json', JSON.stringify(myDb));
+});
+
+process.on('SIGINT', () => {
+    console.log("Storing output");
+    let converted = {};
+    fs.writeFileSync('db/db.json', JSON.stringify(myDb));
+    process.exit();
 });
 
 bot.launch();
